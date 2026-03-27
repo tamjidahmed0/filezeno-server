9cf8a51fd50f1c84f6aef76bcd935c0e92a5169e
Tamjid Ahmed <tamjidahmed050@gmail.com>
2026-03-27 23:47:04 +0600

    chore: install Prisma and Swagger packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Filezeno')
+    .setDescription('The Filezeno API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
